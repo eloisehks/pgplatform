55a8daef74a0e46186fe9769f0b49ba839d41a73
Eloise (MarchBig) <eloiseha@gmail.com>
2024-09-28 23:41:18 +0800

    2nd commit

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,39 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+// ====== scroll top js
+function scrollTo(element, to = 0, duration= 1000) {
+
+	const start = element.scrollTop;
+	const change = to - start;
+	const increment = 20;
+	let currentTime = 0;
+
+	const animateScroll = (() => {
+
+		currentTime += increment;
+
+		const val = Math.easeInOutQuad(currentTime, start, change, duration);
+
+		element.scrollTop = val;
+
+		if (currentTime < duration) {
+			setTimeout(animateScroll, increment);
+		}
+	});
+
+	animateScroll();
+};
+
+Math.easeInOutQuad = function (t, b, c, d) {
+
+	t /= d/2;
+	if (t < 1) return c/2*t*t + b;
+	t--;
+	return -c/2 * (t*(t-2) - 1) + b;
+};
+
+document.querySelector('.scroll-top').onclick = function () {
+	scrollTo(document.documentElement); 
+}
